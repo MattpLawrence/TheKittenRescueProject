f76334e7356f7d82eadf00679fa0a067115fd6fa
Matthew Lawrence <matt.lawrence@improving.com>
2023-03-15 23:00:42 -0400

    Add ngx mask and basic adopter form.

--- the-kitten-rescue-project/src/app/app.module.ts
+++ the-kitten-rescue-project/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { NgxMaskModule } from 'ngx-mask';
 
 import { AppComponent } from './app.component';
 import { BaseComponent } from './common/components/base/base.component';
@@ -60,6 +60,7 @@ import { AdoptStepperComponent } from './pages/adopt/adopt-form/adopt-stepper/ad
     HttpClientModule,
     ReactiveFormsModule,
     FormsModule,
+    NgxMaskModule.forRoot()
 
   ],
   providers: [],
--- the-kitten-rescue-project/src/app/pages/adopt/adopt-form/adopt-form-adopter-info/adopt-form-adopter-info.component.ts
+++ the-kitten-rescue-project/src/app/pages/adopt/adopt-form/adopt-form-adopter-info/adopt-form-adopter-info.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { BaseComponent } from 'src/app/common/components/base/base.component';
 
@@ -9,11 +10,33 @@ import { BaseComponent } from 'src/app/common/components/base/base.component';
 })
 export class AdoptFormAdopterInfoComponent extends BaseComponent implements OnInit {
 
+  form!: FormGroup;
+  //regex validation
+  emailValidation = "[a-zA-Z0-9.-_]{1,}@[a-zA-Z.-]{2,}[.]{1}[a-zA-Z]{2,}";
+  phoneValidation = "^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4}$";
+
   constructor(
-    private router: Router
+    private router: Router,
+    public formBuilder: FormBuilder,
   ) { super()}
 
   ngOnInit(): void {
+    this.form = new FormGroup({
+      adopterFirstName: new FormControl('', [Validators.required]),
+      adopterLastName: new FormControl('', [Validators.required]),
+      adopterOccupation: new FormControl('', [Validators.required]),
+      adopterPhoneNumber: new FormControl('', [Validators.required]),
+      adopterEmail: new FormControl('', [Validators.required]),
+      adopterEmailConfirm: new FormControl('', [Validators.required]),
+      adopterAddressLine1: new FormControl('', [Validators.required]),
+      adopterAddressLine2: new FormControl(''),
+      adopterCity: new FormControl('', [Validators.required]),
+      adopterState: new FormControl('', [Validators.required]),
+      adopterZip: new FormControl('', [Validators.required]),
+      adopterDOB: new FormControl('', [Validators.required]),
+      homeType: new FormControl('', [Validators.required]),
+
+    })
   }
 
   next = () => {
