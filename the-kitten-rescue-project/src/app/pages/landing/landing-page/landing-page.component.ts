d3a61503c01046846f6d35b8f7adaf867f3dcea7
Matthew Lawrence <matt.lawrence@improving.com>
2023-03-22 17:05:43 -0400

    update landing page style.

--- the-kitten-rescue-project/src/app/pages/landing/landing-page/landing-page.component.ts
+++ the-kitten-rescue-project/src/app/pages/landing/landing-page/landing-page.component.ts
@@ -1,15 +1,33 @@
 import { Component, OnInit } from '@angular/core';
+import { BaseComponent } from 'src/app/common/components/base/base.component';
 
 @Component({
   selector: 'app-landing-page',
   templateUrl: './landing-page.component.html',
   styleUrls: ['./landing-page.component.scss']
 })
-export class LandingPageComponent implements OnInit {
+export class LandingPageComponent extends BaseComponent implements OnInit {
 
-  constructor() { }
+  currentString: string = 'From rescued to adopted - Helping kittens find their way home, one paw at a time.'
+  // initialString: string = 'Helping kittens find their way home, one paw at a time.'
+  i:number = 0;
+  constructor() {super() }
 
   ngOnInit(): void {
+    // this.initTypeWriter()
   }
+
+  // initTypeWriter = () => {
+
+
+  //   if(this.i < this.initialString.length){
+  //     //add character
+  //     this.currentString += this.initialString.charAt(this.i);
+  //     //go to next character
+  //     this.i++;
+  //     //pause then call function again until last character
+  //     setTimeout(() => this.initTypeWriter(), 50)
+  //   }
+  // }
 
 }
