44652dff4be4e6d1b7b4799c33329a400eb23559
Matthew Lawrence <matt.lawrence@improving.com>
2023-03-22 19:01:10 -0400

    add card styles.

--- the-kitten-rescue-project/src/app/pages/landing/landing-cards/landing-cards.component.ts
+++ the-kitten-rescue-project/src/app/pages/landing/landing-cards/landing-cards.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { BaseComponent } from 'src/app/common/components/base/base.component';
 
 @Component({
   selector: 'app-landing-cards',
   templateUrl: './landing-cards.component.html',
   styleUrls: ['./landing-cards.component.scss']
 })
-export class LandingCardsComponent implements OnInit {
+export class LandingCardsComponent extends BaseComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private router: Router
+  ) {super() }
 
   ngOnInit(): void {
+  }
+
+  navigate = (page:string) => {
+    this.router.navigate([page]);
   }
 
 }
