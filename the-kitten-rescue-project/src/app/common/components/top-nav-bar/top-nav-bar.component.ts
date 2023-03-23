a7aa91cc7f55e455801868c01355aeb953fce97f
Matthew Lawrence <matt.lawrence@improving.com>
2023-03-23 10:53:11 -0400

    update style and add scroll to top header.

--- the-kitten-rescue-project/src/app/common/components/top-nav-bar/top-nav-bar.component.ts
+++ the-kitten-rescue-project/src/app/common/components/top-nav-bar/top-nav-bar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener  } from '@angular/core';
 import { BaseComponent } from '../base/base.component';
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { distinctUntilChanged, takeUntil, tap } from 'rxjs';
@@ -14,6 +14,8 @@ export class TopNavBarComponent extends BaseComponent implements OnInit {
 
   currentBreakpoint: BreakPointsEnum = BreakPointsEnum.isDesktop;
   isExpanded: boolean = false;
+  prevY = window.scrollY;
+  isHidden: boolean = false;
 
   constructor(
     private breakpointObserver: BreakpointObserver,
@@ -22,14 +24,26 @@ export class TopNavBarComponent extends BaseComponent implements OnInit {
     super();
    }
 
-
-   
   readonly breakpoint$ = this.breakpointObserver
   .observe([Breakpoints.XLarge, Breakpoints.Large, Breakpoints.Medium, Breakpoints.Small, '(min-width: 500px)'])
   .pipe(
     tap(),
     distinctUntilChanged()
   );
+
+  @HostListener('window:scroll', ['$event'])
+  onScroll(event:any){
+    console.log(window.scrollY)
+    //compare current scroll position to previous
+    let currentY = window.scrollY;
+    if( this.prevY > currentY){
+      this.isHidden = false;
+    } else{
+      this.isHidden = true;
+    }
+    //set scroll position
+    this.prevY = currentY
+  }
 
   ngOnInit(): void {
     this.initBreakpoints();
@@ -38,6 +52,7 @@ export class TopNavBarComponent extends BaseComponent implements OnInit {
   initBreakpoints = () => {
     this.breakpoint$.pipe(takeUntil(this.ngUnsubscribe)).subscribe(() => this.breakpointChanged())
   }
+
 
   breakpointChanged = () => {
     let currentBreakpoint: BreakPointsEnum = BreakPointsEnum.isDesktop;
--- the-kitten-rescue-project/src/app/pages/about/about-tabs/about-tabs.component.ts
+++ the-kitten-rescue-project/src/app/pages/about/about-tabs/about-tabs.component.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
+import { takeUntil } from 'rxjs';
+import { BaseComponent } from 'src/app/common/components/base/base.component';
+import { BreakPointsEnum } from 'src/app/common/models/common.enum';
+import { CommonService } from 'src/app/common/services/common.service';
 
 @Component({
   selector: 'app-about-tabs',
   templateUrl: './about-tabs.component.html',
   styleUrls: ['./about-tabs.component.scss']
 })
-export class AboutTabsComponent implements OnInit {
+export class AboutTabsComponent extends BaseComponent implements OnInit {
 
-  constructor() { }
+  currentBreakPoint: BreakPointsEnum = 0;
+
+  constructor(
+    private commonService: CommonService
+  ) { super() }
 
   ngOnInit(): void {
+    this.initBreakpoint();
+  }
+
+  initBreakpoint = () => {
+    this.commonService.getBreakpointSubject().pipe(takeUntil(this.ngUnsubscribe)).subscribe(res => {
+      this.currentBreakPoint = res;
+    })
   }
 
 }
