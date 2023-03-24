d52858afeff89d42beaa03443b6313109acd4ab3
Matthew Lawrence <matt.lawrence@improving.com>
2023-03-24 13:06:39 -0400

    add footer.

--- the-kitten-rescue-project/src/app/common/components/footer/footer.component.ts
+++ the-kitten-rescue-project/src/app/common/components/footer/footer.component.ts
@@ -1,24 +1,42 @@
 import { Component, OnInit } from '@angular/core';
-import { faShieldCat} from '@fortawesome/free-solid-svg-icons';
+import { faEnvelope} from '@fortawesome/free-regular-svg-icons';
 import { faYoutube, faTiktok, faFacebook, faInstagram} from '@fortawesome/free-brands-svg-icons';
+import { BaseComponent } from '../base/base.component';
+import { CommonService } from '../../services/common.service';
+import { takeUntil } from 'rxjs';
+import { BreakPointsEnum } from '../../models/common.enum';
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.scss']
 })
-export class FooterComponent implements OnInit {
+export class FooterComponent extends BaseComponent implements OnInit {
 
   icons = {
     youtube: faYoutube,
     tiktok: faTiktok,
     facebook: faFacebook,
-    instagram: faInstagram
+    instagram: faInstagram,
+    email: faEnvelope
   }
 
-  constructor() { }
+  currentBreakpoint: BreakPointsEnum = 0;
+
+  constructor(
+    private commonService: CommonService
+  ) {super() }
 
   ngOnInit(): void {
+
+    this.initBreakpoint();
+  }
+
+  initBreakpoint = () => {
+    this.commonService.getBreakpointSubject().pipe(takeUntil(this.ngUnsubscribe)).subscribe(res => {
+      this.currentBreakpoint = res;
+      console.log(res)
+    })
   }
 
 }
