f901c6bfb310fc054c9ba1389d5a3d2c8bd21b63
Matthew Lawrence <matt.lawrence@improving.com>
2023-11-20 11:09:15 -0500

    update send api url

--- the-kitten-rescue-project/src/environments/environment.ts
+++ the-kitten-rescue-project/src/environments/environment.ts
@@ -6,6 +6,5 @@ export const environment = {
   production: false,
   API_KEY: "iqhIcRJ9JJslUyrAkophmSMaw4MCbYMM85pugSbMiz6YWiOoY1",
   API_SECRET: "UjPsYnNTKtA7j2LBxEW8O1pbOkbu8KvyVtXmHtQ6",
-  // API_URL: "https://the-kitten-api.netlify.app/.netlify/functions/server"
-  API_URL: "http://localhost:9000/.netlify/functions/server"
+  API_URL: "https://the-kitten-api.netlify.app/.netlify/functions/server"
 };
